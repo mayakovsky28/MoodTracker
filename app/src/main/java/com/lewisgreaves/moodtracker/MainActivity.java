e181e5fdc35249783c4d672abccae9bff0df59fd
Konstantin <lewiswilliamgreaves@gmail.com>
2019-09-06 15:32:30 +0100

    Started to implement methods to register user swipes.

--- app/src/main/java/com/lewisgreaves/moodtracker/MainActivity.java
+++ app/src/main/java/com/lewisgreaves/moodtracker/MainActivity.java
@@ -3,12 +3,77 @@ package com.lewisgreaves.moodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    public static final int MOVEMENT_THRESHOLD = 100;
+    public static final int SWIPE_VELOCITY_THRESHOLD = 100;
+    public static final int SWIPE_THRESHOLD = 100;
+    private ImageButton mHomePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHomePage = findViewById(R.id.homepage);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
+        boolean result = false;
+        float diffY = moveEvent.getY() - downEvent.getY();
+        float diffX = moveEvent.getX() - downEvent.getX();
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            //horizontal swipe
+            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffX > 0) {
+                    onSwipeRight();
+                } else {
+                    onSwipeLeft();
+                }
+            }
+        } else {
+            //vertical swipe
+            if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+
+            }
+        }
+        return result;
+    }
+
+    private void onSwipeLeft() {
+    }
+
+    private void onSwipeRight() {
     }
 }
