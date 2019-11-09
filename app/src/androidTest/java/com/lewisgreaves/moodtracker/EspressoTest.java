4da6c43afdd1da1124a9240da28bd63afdaa03b4
Konstantin <lewiswilliamgreaves@gmail.com>
2019-11-09 10:34:14 +0000

    Added Text View test matcher. Added test for Toast.

--- app/src/androidTest/java/com/lewisgreaves/moodtracker/EspressoTest.java
+++ app/src/androidTest/java/com/lewisgreaves/moodtracker/EspressoTest.java
@@ -4,7 +4,10 @@ package com.lewisgreaves.moodtracker;
  * Created by @Mayakovsky28 on 23 10 2019.
  */
 
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.espresso.action.TypeTextAction;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -19,19 +22,51 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.app.PendingIntent.getActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeDown;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.hasBackground;
 import static androidx.test.espresso.matcher.ViewMatchers.hasFocus;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.lewisgreaves.moodtracker.EspressoTestsMatchers.withDrawable;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
+
+    Matcher<View> textViewHasValue() {
+
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("The TextView/EditText has value");
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextView) && !(view instanceof EditText)) {
+                    return false;
+                }
+                if (view != null) {
+                    String text;
+                    if (view instanceof TextView) {
+                        text = ((TextView) view).getText().toString();
+                    } else {
+                        text = ((EditText) view).getText().toString();
+                    }
+
+                    return (!TextUtils.isEmpty(text));
+                }
+                return false;
+            }
+        };
+    }
 
     @Rule
     public ActivityTestRule mActivityRule = new ActivityTestRule(MainActivity.class);
@@ -49,7 +84,7 @@ public class EspressoTest {
         onView(withId(R.id.add_note)).perform(click());
         onView(ViewMatchers.withText("Cancel")).perform(click());
         onView(withId(R.id.add_note)).perform(click());
-//        TODO: onView(withId(R.id.noteEditText)).... how to get the text...https://www.google.com/url?q=https://stackoverflow.com/a/45601564/5066615&sa=D&source=hangouts&ust=1573077736502000&usg=AFQjCNHV_b_OljSD6wzL5DUgev6iNPU_Ug
+        onView(withId(R.id.noteEditText)).check(matches(textViewHasValue()));
         onView(ViewMatchers.withText("Describe")).check(matches(isDisplayed())).perform(typeText("Just got a date!"));
         onView(ViewMatchers.withText("OK")).perform(click());
         onView(withId(R.id.homepage)).check(matches(ViewMatchers.withText("")));
@@ -61,6 +96,12 @@ public class EspressoTest {
     public void tappingViewHistoryChangesActivity() {
         onView(withId(R.id.view_history)).perform(click());
         onView(withId(R.id.mood_recycler_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void tappingNoteIconInRecyclerViewShowsNoteToast(){
+        onView(withId(R.id.view_history)).perform(click());
+        onView(withId(R.id.noteImageView)).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView()))).check(matches(isDisplayed()));
     }
 
     @Test
